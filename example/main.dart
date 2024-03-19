da7160872e65fa5e50c83576a1b29963ab39e26a
Andrii Syrokomskyi <signmotion@gmail.com>
2024-03-19 21:10:35 +0100

    Added an example.

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:data_pipe/data_pipe.dart';
 
 void main() {
-  // TODO
+  // pump to List<int>
+  final file = File('./example/1.webp');
+  final r = (D(file) | const D(<int>[]));
+  print(r.data);
 }
