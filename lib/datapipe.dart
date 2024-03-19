d7250c162f07b9ea65a3157f4b80be839ecc2573
Andrii Syrokomskyi <signmotion@gmail.com>
2024-03-19 22:49:47 +0100

    Renamed folder `pipes` to `pumps`.

--- lib/datapipe.dart
+++ lib/datapipe.dart
@@ -10,10 +10,10 @@ import 'src/constants.dart';
 part 'src/extensions/pipe.dart';
 part 'src/extensions/save.dart';
 
-part 'src/pipes/directory_to_dart_const_tags_bytes.dart';
-part 'src/pipes/file_to_dart_const_list_int.dart';
-part 'src/pipes/file_to_list_int.dart';
-part 'src/pipes/list_int_to_file.dart';
+part 'src/pumps/directory_to_dart_const_tags_bytes.dart';
+part 'src/pumps/file_to_dart_const_list_int.dart';
+part 'src/pumps/file_to_list_int.dart';
+part 'src/pumps/list_int_to_file.dart';
 
 part 'src/pipe.dart';
 part 'src/own_type.dart';
