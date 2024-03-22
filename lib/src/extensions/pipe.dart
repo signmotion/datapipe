43e339b8a5c6017dafa7264dfc5cf10aa4831a06
Andrii Syrokomskyi <signmotion@gmail.com>
2024-03-22 15:09:09 +0100

    Commented.

--- lib/src/extensions/pipe.dart
+++ lib/src/extensions/pipe.dart
@@ -1,7 +1,11 @@
 part of '../../datapipe.dart';
 
 extension PipeExt<T> on T {
+  /// Alias for constructing [Pipe].
+  /// See [oo].
   Pipe<T, PipeOptions> get o => Pipe(this);
 
+  /// Alias for constructing [Pipe] with options.
+  /// See [o].
   Pipe<T, PipeOptions> oo(PipeOptions options) => Pipe(this, options: options);
 }
